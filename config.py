b2e677c540906042cfd4c2572ec215656511d006
lazzzyone <32535360+lazzzyone@users.noreply.github.com>
2020-04-22 03:39:22 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,10 @@
-token = 'ada1683e9ca915298b1fc2456942c0b240f6135c1fd32e4a8518a581a733c2571727c301b11fe5986fda1'
+TOKEN = 'ada1683e9ca915298b1fc2456942c0b240f6135c1fd32e4a8518a581a733c2571727c301b11fe5986fda1'
+
+LOGIN = 'Babaev1'
+PASSWORD = 'Babaev0119'
+
+ELSCHOOL = 'https://elschool.ru/logon/index'
+DIARYPAGE = 'https://elschool.ru/users/diaries/details?rooId=53&instituteId=447&departmentId=76620&pupilId=451153'
+
+TIMETABLE_URL = 'http://licey83.ru/images/raspisanie/ras%D1%80.JPG'
+BELLS_URL = 'http://licey83.ru/images/rasp_urukov2020.JPG'
