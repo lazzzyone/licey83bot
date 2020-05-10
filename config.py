96ebfaaf9cc69517304998b9cab5d9ff6b59826f
lazzzyone <babaev.john@gmail.com>
2020-05-10 20:00:31 +0500

    timetable link added

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ PASSWORD = 'Babaev0119'
 ELSCHOOL = 'https://elschool.ru/logon/index'
 DIARYPAGE = 'https://elschool.ru/users/diaries/details?rooId=53&instituteId=447&departmentId=76620&pupilId=451153'
 
-TIMETABLE_URL = ['http://licey83.ru/images/raspisanie/ras%D1%801.JPG']
+TIMETABLE_URL = ['http://licey83.ru/images/raspisanie/ras%D1%801.JPG','http://licey83.ru/images/raspisanie/rasp2.JPG']
 BACKUP_TIMETABLE_URL = 'http://licey83.ru/images/raspisanie/ras%D1%80.JPG'
 BELLS_URL = 'http://licey83.ru/images/rasp_urukov2020.JPG'
